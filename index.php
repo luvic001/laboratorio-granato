ea5848df70a899dbee7c732213b07732a7627898
lucasVictor <lucas.v0697@gmail.com>
2021-06-04 16:14:34 -0300

    Exames de Rotina

--- index.php
+++ index.php
@@ -8,5 +8,6 @@ get_modules('webdoor', 'home');
 get_modules('about-us', 'home');
 get_modules('testes', 'home');
 get_modules('testes-carousel', 'home');
+get_modules('resultados', 'home');
 
 get_footer();
---
+++ modules/home/resultados.php
@@ -0,0 +1,72 @@
+<?php
+
+if (!defined('PATH')) exit;
+
+?>
+
+<section class="about-us site-sections" id="about-us">
+
+  <div class="container">
+    <div class="section-title">
+      <h2>RESULTADOS RÁPIDOS <br> <span>EXAMES DE ROTINA</span></h2>
+    </div>
+    <div class="row list-site">
+      <div class="col-12 editor-content mb-3 mb-md-4">
+        <p>Realizamos ampla gama de exames laboratoriais para o mesmo dia. <br/>Confira abaixo.</p>
+      </div>
+      <div class="col-md-3 col-6">
+        <ul>
+          <li>HEMOGRAMA</li> 
+          <li>RETICULÓCITOS </li> 
+          <li>HB GLICOSILADA </li> 
+          <li>TAP </li> 
+          <li>PTT </li> 
+          <li>D DIMERO </li> 
+          <li>FIBRINOGÊNIO </li> 
+          <li>GRUPO RH </li> 
+          <li>GLICOSE</li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-6">
+        <ul>
+          <li> UREIA </li>
+          <li>CREATININA </li>
+          <li>AC URICO </li>
+          <li>TGO </li>
+          <li>TGP </li>
+          <li>GGT </li>
+          <li>FOSFATASE </li>
+          <li>ALCALINA </li>
+          <li>PROTEINAS TOTAIS E FRAÇÕES</li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-6">
+        <ul> 
+          <li>BILIRRUBINAS </li>
+          <li>COLESTEROL TOTAL E FRAÇÕES </li>
+          <li>LIPIDOGRAMA </li>
+          <li>AMILASE </li>
+          <li>LIPASE </li>
+          <li>LDH </li>
+          <li>CPK </li>
+          <li>CKMB</li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-6">
+        <ul> 
+          <li>FERRO </li>
+          <li>TRANSFERRINA </li>
+          <li>FERRITINA </li>
+          <li>SÓDIO </li>
+          <li>POTÁSSIO </li>
+          <li>EAS </li>
+          <li>PPF </li>
+          <li>BETA HCG </li>
+          <li>SOROLOGIA IGG E IGM</li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+
+</section>
