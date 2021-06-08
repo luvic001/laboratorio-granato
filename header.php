b89d9118b58e75c510cdd1edc1b12a87f65a6705
lucasVictor <lucas.v0697@gmail.com>
2021-06-08 16:34:18 -0300

    Google Pagespeed

--- header.php
+++ header.php
@@ -14,14 +14,26 @@ $sitename = get_bloginfo('name') . ' | ' . get_bloginfo('description');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= wp_title() ?></title>
   <?php wp_head() ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-199048794-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-199048794-1');
+  </script>
 
   <!-- Files -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-  <link href="<?= $TEMPLATE_DIRECTORY_URI ?>/style.css" rel="stylesheet">
+  <link data-href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <style>
+    <?= get_file('style.css') ?>
+  </style>
   <script type="text/javascript" src="<?= $TEMPLATE_DIRECTORY_URI ?>/js/jQuery.min.js"></script>
-  <script type="text/javascript" src="<?= $TEMPLATE_DIRECTORY_URI ?>/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="<?= $TEMPLATE_DIRECTORY_URI ?>/js/script.min.js"></script>
+  <script type="text/javascript" src="<?= $TEMPLATE_DIRECTORY_URI ?>/js/bootstrap.min.js" async defer></script>
+  <script type="text/javascript" src="<?= $TEMPLATE_DIRECTORY_URI ?>/js/script.min.js" async defer></script>
+
   <!-- / End Files -->
 
 </head>
