8e4a6076b55a4a15eaba08fc7d2634a4bd47fb37
lucasVictor <lucas.v0697@gmail.com>
2021-08-09 11:16:18 -0300

    Solicitações Bruno

--- modules/home/webdoor.php
+++ modules/home/webdoor.php
@@ -16,7 +16,7 @@ $repeat = 1;
           <div class="section-lateral-paddings h-100">
             <div class="container-fluid d-flex align-items-center h-100">
               <div class="slide-wrapper">
-                <h2>VOLTE ÀS <br> ATIVIDADES <br> COM SEGURANÇA</h2>
+                <h2>VOLTE ÀS ATIVIDADES <br> COM SEGURANÇA</h2>
                 <p>Teste PCR para empresas com</p>
                 <p>resultado no mesmo dia</p>
                 <a href="#testes-para-covid-19" class="btn-site white-button bt_webdoor" title="Saiba mais">Saiba mais <?= svg('arrow-right') ?></a>
